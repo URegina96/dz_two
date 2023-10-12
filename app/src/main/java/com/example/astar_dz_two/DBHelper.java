557d35b883aaadf1de99a49f2d0412f4597e4210
regina <usmanova.re@yandex.ru>
2023-10-12 23:56:29 +0500

    public class DBHelper
    //шаг-6.4 реализация логики создания таблиц и заполнения данными при помощи специальных команд SQL
    //шаг -6.5 запрос на дроп таблицы, затем можно вызвать метод создания новой версии таблицы с обновлённой структурой

--- app/src/main/java/com/example/astar_dz_two/DBHelper.java
+++ app/src/main/java/com/example/astar_dz_two/DBHelper.java
@@ -18,32 +18,25 @@ public class DBHelper extends SQLiteOpenHelper {//шаг-6 создается к
 
 
     public DBHelper(@Nullable Context context, @Nullable String name, int version) {    //6.2- реализовываем конструктор
-        super(context, name, null, version);
+        super(context, DATABASE_NAME, null, DATABASE_VERSION); //используем константы
     }
 
     @Override//шаг-6.1 реализуется два обязательных абстракных метода с аннотаций   @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {//метод вызывается при создании базы данных
+        //шаг-6.4 реализация логики создания таблиц и заполнения данными при помощи специальных команд SQL
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_CONTACTS + " (" + KEY_ID
+                + " INTEGER PRIMARY KEY, " + KEY_NAME + " TEXT, " + KEY_AGE + " TEXT)");
 
     }
 
     @Override//шаг-6.1 реализуется два обязательных абстракных метода с аннотаций   @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {//метод вызывается при изменени в базе данных ( а именно удаление и востановление - это когда меняется версия бд)
-
+        //шаг -6.5 запрос на дроп таблицы, затем можно вызвать метод создания новой версии таблицы с обновлённой структурой
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+        onCreate(sqLiteDatabase);
     }
-
-
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
 //6.2 в конструкторе суперкласса передаем параметры (контекст, имя базы данных, объект класса курсов factory (на данный момент заnullим его так как не будем использовать), версия базы данных)
+//drop table -  запрос на уничтожение таблицы базы данных
