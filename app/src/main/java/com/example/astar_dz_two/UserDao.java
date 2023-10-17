79596bbaf11d49578b6a3ed93d8a5e16e4dec99f
regina <usmanova.re@yandex.ru>
2023-10-17 17:11:10 +0500

    шаг 18 – создаем UserDao - данный интерфейс просто предоставляет методы для действий с пользователями

---
+++ app/src/main/java/com/example/astar_dz_two/UserDao.java
@@ -0,0 +1,8 @@
+package com.example.astar_dz_two;
+
+import java.util.List;
+
+public class UserDao { //шаг 18 – создаем UserDao
+    List<User> getUsers;//шаг 18.1 – получаем весь список пользователей
+
+}
